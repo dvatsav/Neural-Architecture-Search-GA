8b9cc65fe2ed2d9b10a4c5d4ac328ea26c6bf176
dvatsav <deepak.srivatsav@gmail.com>
2019-12-01 17:30:30 +0530

    lambda key name

--- search.py
+++ search.py
@@ -39,7 +39,7 @@ algorithm = NSGA2(pop_size=5, eliminate_duplicates=True, n_offsprings=1)
 result = minimize(problem, algorithm, termination=('n_gen', 5))
 logging.info("Best Genome _ %s"%(str(result.X)))
 performances = problem.model_performances
-best_performance_model = sorted(performances.items(), key=lambda kv:kv[1]['test_accuracy'], reverse=True)[0]
+best_performance_model = sorted(performances.items(), key=lambda kv:kv[1]['test accuracy'], reverse=True)[0]
 print (best_performance_model)
 best_model = 0
 for model in best_performance_model:
